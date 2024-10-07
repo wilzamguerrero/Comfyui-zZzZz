b2524bbd9443060fc08da9318ce6d0808896dc9e
wilzamguerrero <110712636+wilzamguerrero@users.noreply.github.com>
2024-10-06 17:41:54 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,15 +1,21 @@
 from .DownloadFileNode import DownloadFileNode
 from .CompressFolderNode import CompressFolderNode
-from .MoveZNode import MoveZNode  # Importa el nodo Move Z  # Importa el nuevo nodo
+from .MoveZNode import MoveZNode
+from .DeleteZNode import DeleteZNode  # Importa el nodo Delete
+from .RenameZNode import RenameZNode  # Importa el nodo Rename
 
 NODE_CLASS_MAPPINGS = {
     "DownloadFileNode": DownloadFileNode,
-    "CompressFolderNode": CompressFolderNode,  # Agrega el nuevo nodo
-    "MoveZNode": MoveZNode,  # Agrega el nodo Move Z
+    "CompressFolderNode": CompressFolderNode,
+    "MoveZNode": MoveZNode,
+    "DeleteZNode": DeleteZNode,  # Agrega el nodo Delete
+    "RenameZNode": RenameZNode,  # Agrega el nodo Rename
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "DownloadFileNode": "Download Z",
     "CompressFolderNode": "Compress Z",
-    "MoveZNode": "Move Z"  # Agrega el nombre del nuevo nodo para la UI
+    "MoveZNode": "Move Z",
+    "DeleteZNode": "Delete Z",  # Agrega el nombre del nodo Delete para la UI
+    "RenameZNode": "Rename Z"   # Agrega el nombre del nodo Rename para la UI
 }
