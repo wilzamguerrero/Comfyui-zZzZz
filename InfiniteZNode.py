34e8feb99c56e68a77514cb771d5e08897e4ca24
wilzamguerrero <110712636+wilzamguerrero@users.noreply.github.com>
2024-10-11 15:51:27 -0700

    Update InfiniteZNode.py

--- InfiniteZNode.py
+++ InfiniteZNode.py
@@ -8,8 +8,8 @@ class InfiniteZNode:
     def INPUT_TYPES(s):
         return {
             "required": {
-                "port": ("STRING", {"default": "7888"}),
-                "tunnel_url": ("STRING", {"default": ""}),  # Nueva opción para la URL del túnel
+                "port": ("STRING", {"default": "8080"}),  # Cambia el puerto para el nodo Infinite
+                "tunnel_url": ("STRING", {"default": "http://localhost"})  # El mismo túnel de ComfyUI
             }
         }
 
@@ -18,11 +18,11 @@ class InfiniteZNode:
     CATEGORY = "zZzZz"
     OUTPUT_NODE = True
 
-    def execute_infinite_browser(self, port, tunnel_url=""):
+    def execute_infinite_browser(self, port, tunnel_url="http://localhost"):
         try:
-            # Ruta al archivo app.py
+            # Definir la ruta del nodo y subir dos niveles
             node_path = os.path.dirname(os.path.abspath(__file__))
-            root_path = os.path.abspath(os.path.join(node_path, "..", "..", ".."))  # Subir tres niveles
+            root_path = os.path.abspath(os.path.join(node_path, "..", ".."))
             app_py_path = os.path.join(root_path, "app.py")
 
             if not os.path.exists(app_py_path):
@@ -33,13 +33,9 @@ class InfiniteZNode:
 
             # Ejecutar app.py desde la raíz
             subprocess.Popen([python_command, app_py_path, f"--port={port}"], cwd=root_path)
-
-            # Si se proporcionó una URL de túnel, usarla
-            if tunnel_url:
-                webbrowser.open_new_tab(f"{tunnel_url}")
-            else:
-                webbrowser.open_new_tab(f"http://localhost:{port}")
-
+            
+            # Abrir el sublink generado por el túnel pero con el puerto del nodo Infinite
+            webbrowser.open_new_tab(f"{tunnel_url}:{port}")
             return ()
         except Exception as e:
             print(f"Error: {e}")
