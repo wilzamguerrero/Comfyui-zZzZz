6cef18fb8b7091238ad8d6ade1d32996f4aed87e
wilzamguerrero <110712636+wilzamguerrero@users.noreply.github.com>
2024-10-13 12:03:35 -0700

    Update InfiniteZNode.py v00 again

--- InfiniteZNode.py
+++ InfiniteZNode.py
@@ -38,15 +38,17 @@ class InfiniteZNode:
 
     def execute_infinite_browser(self, port, tunnel_option="LocalTunnel"):
         try:
-            app_py_path = "/kaggle/working/SDZC/app.py"
-    
+            node_path = os.path.dirname(os.path.abspath(__file__))
+            root_path = os.path.abspath(os.path.join(node_path, "..", ".."))
+            app_py_path = os.path.join(root_path, "app.py")
+
             if not os.path.exists(app_py_path):
                 error_message = f"Error: {app_py_path} no existe."
                 print(error_message)
                 return (error_message,)
-    
+
             python_command = "python" if platform.system() == "Windows" else "python3"
-            subprocess.Popen([python_command, app_py_path, f"--port={port}"], cwd="/kaggle/working/SDZC")
+            subprocess.Popen([python_command, app_py_path, f"--port={port}"], cwd=root_path)
 
             if tunnel_option == "LocalTunnel":
                 return self.local_tunnel(port)
@@ -221,7 +223,6 @@ class InfiniteZNode:
             error_message = f"Error: {e}"
             print(error_message)
             return (error_message,)
-
 
 
 
